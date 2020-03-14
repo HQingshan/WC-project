efa9edf41e54dccf9158ea12e86698b88c2c3a1c
黄清山 <1076479532@qq.com>
2020-03-15 04:37:39 +0800

    个人项目第一次提交

---
+++ wc/wc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  wc
+//
+//  Created by 你好帅 on 2020/3/15.
+//  Copyright © 2020 你好帅. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
